7fadeba2a4ce1ced70399fce227679202b316bc6
Owen Brown <owbr9203@colorado.edu>
2025-04-02 15:19:58 -0600

    Random player pt1

---
+++ RandomPlayer.py
@@ -0,0 +1,38 @@
+import mancala as m
+
+class RandomPlayer():
+    def __init__(self):
+        """
+        Initializes a Random Player that plays mancala
+        """
+        self.num_wins = 0
+        self.num_loss = 0
+        self.num_tie = 0
+        self.turns = 0
+    
+    def getAvgTurns(self):
+        """
+        Calculates the average turns the player takes by dividing turns by number of games
+        """
+        return self.turns/(self.num_wins+self.num_loss+self.num_tie)
+    
+    def getAvgWins(self):
+        """
+        Calculates the average wins by dividing wins by number of games
+        """
+        return self.num_wins/(self.num_wins+self.num_loss+self.num_tie)
+    
+    def getAvgLosses(self):
+        """
+        Calculates the average loss by dividing losses by number of games
+        """
+        return self.num_loss/(self.num_wins+self.num_loss+self.num_tie)
+    
+    def getAvgTies(self):
+        """
+        Calculates the average ties by dividing ties by number of games
+        """
+        return self.num_tie/(self.num_wins+self.num_loss+self.num_tie)
+    
+    def move(self,game):
+        game.play(game.random_move_generator())
