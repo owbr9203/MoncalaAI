4da4a0c30862d883f138f0d5de9986ad509259cc
Tai <fpolanow@gmail.com>
2025-04-02 15:50:58 -0600

    main skeleton

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import mancala as m
+import RandomPlayer as player
+
+
+def play_game(player1, player2, game):
+    playing = True
+    while(playing):
+        player1.play()
+        player2.play()
+        playing = False
+        res = 1 # player 1 won
+    return res
+
+
+def main():
+    
+    player1, player2 = player.RandomPlayer(), player.RandomPlayer()
+
+    for i in range(100):
+        game = m.Mancala
+        print(play_game(player1, player2, game))
+
+        
+if __name__ == "__main__":
+    main()
 
 
