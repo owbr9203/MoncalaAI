16f595f4c6d9265210d1660969afa04afc3ef98e
Owen Brown <owbr9203@colorado.edu>
2025-04-15 11:48:30 -0600

    Edited methods to all be 0-index, changed utility to work with marble difference, fixed max and min value, added depth feature and deep copy. Added method for playing alphabeta game in main.

--- main.py
+++ main.py
@@ -44,13 +44,13 @@ def play_rand_game(player1: player, player2: player, game:m):
             curr_player = 1
     return turns
 
-def play_game_alphabeta(player1: player, player2: player, game:m):
+def play_game_alphabeta(player1: player, player2: player, game:m,depth: int=30):
     playing = True
     curr_player = 1
     turns = 0
     
     while(playing):
-        status = game.play(game.alphabeta_search())
+        status = game.play(game.alphabeta_search(depth))
         # print(status, "stones: ", find_total_stones(game))
         # game.display_board()
         turns+=1
@@ -79,17 +79,53 @@ def play_game_alphabeta(player1: player, player2: player, game:m):
             curr_player = 1
     return turns
 
+def play_game_alphabeta_rand(player1: player, player2: player, game:m, depth: int = 10):
+    playing = True
+    curr_player = 1
+    turns = 0
+    
+    while(playing):
+        if curr_player==1:
+            status = game.play(game.alphabeta_search(depth))
+        if curr_player==2:
+            status = game.play(game.random_move_generator())
+        # print(status, "stones: ", find_total_stones(game))
+        # game.display_board()
+        turns+=1
+        if status < 0 :
+            print("Failed")
+            return turns
+        
+        if status > 0:
+            print("Finished game")
+            match status:
+                case 1:
+                    player1.num_wins += 1
+                    player2.num_loss += 1
+                    break
+                case 2:
+                    player1.num_loss += 1
+                    player2.num_wins += 1
+                    break
+                case _:
+                    player1.num_tie += 1
+                    player2.num_tie += 1  
+                    break   
+        if curr_player == 1:
+            curr_player = 2
+        else:
+            curr_player = 1
+    return turns
 
 
 def main():
     
     player1, player2 = player.Player(), player.Player()
     total_turns = 0
-    game = m.Mancala()
-    """ for i in range(100):
+    for i in range(100):
         
         game = m.Mancala()
-        total_turns += play_rand_game(player1, player2, game)
+        total_turns += play_game_alphabeta_rand(player1, player2, game, 10)
         
         
         print("==============================player1==============================")
@@ -98,8 +134,8 @@ def main():
         player2.display_stats()
         print("=============================game stats============================")
         print("average turns per game: {}".format(total_turns/(i+1)))
-        # print(game.display_board()) """
-    play_game_alphabeta(player1,player2,game)
+        # print(game.display_board())
+        print(i)
     print(game.display_board())
         
 if __name__ == "__main__":
--- mancala.py
+++ mancala.py
@@ -1,7 +1,7 @@
 import random
 from random import choice
 import time
-import sys
+import copy
 random.seed(time.process_time_ns())  # Seeds the random number generator for reproducibility
 
 class Mancala:
@@ -18,6 +18,7 @@ class Mancala:
         self.p1_mancala_index = self.pits_per_player  # Index for player 1's mancala
         self.p2_pits_index = [self.pits_per_player+1, len(self.board)-1-1]  # Indexes for player 2's pits
         self.p2_mancala_index = len(self.board)-1  # Index for player 2's mancala
+        self.stones_per_pit = stones_per_pit
         
         # Zeroing the Mancala for both players
         self.board[self.p1_mancala_index] = 0  # Sets player 1's mancala to 0
@@ -48,25 +49,26 @@ class Mancala:
         
     def valid_move(self, pit):
         """
-        Checks if the pit chosen by the current_player is a valid move.
+        Checks if the pit chosen by the current_player is a valid move. TAKES IN 0-index for where pit is on board
         """
-        
-        if pit < 0 or pit >= len(self.board): return False  # Checks if the pit is within the board bounds
-        if pit in [self.p1_mancala_index, self.p2_mancala_index] or self.board[pit] <= 0: return False  # Checks if the pit is a mancala or is empty
-        
+        if self.current_player==1: #For P1
+            if pit<0 or pit>=self.p1_mancala_index or self.board[pit]<=0: #If not one of P1's pits or empty...
+                return False #Invalid
+        else: #For P2
+            if pit<=self.p1_mancala_index or pit>=self.p2_mancala_index or self.board[pit]<=0: #If not one of P2's pits or empty...
+                return False #Invalid
         return True  # If all checks pass, the move is valid
         
     def valid_moves(self):
         pits = []
-        if self.current_player == 1:
-            
-            for pit in range(self.p1_mancala_index):
-                if self.board[pit] > 0 and pit != self.p1_mancala_index and pit != self.p2_mancala_index:
-                    pits.append(pit)
-        else:
-            for pit in range(self.p1_mancala_index + 1, self.p2_mancala_index):
-                if self.board[pit] > 0 and pit != self.p1_mancala_index and pit != self.p2_mancala_index:
-                    pits.append(pit)
+        if self.current_player == 1: #For P1
+            for pit in range(self.p1_mancala_index): #For every P1 pit other than their mancala...
+                if self.board[pit] > 0: #If that pit has stones in it
+                    pits.append(pit) #Add to valid pits
+        else: #For P2
+            for pit in range(self.p1_mancala_index + 1, self.p2_mancala_index): #For every P2 pit other than their mancala...
+                if self.board[pit] > 0: #If that pit has stones in it
+                    pits.append(pit) #Add to valid pits
         return pits
 
     def random_move_generator(self):
@@ -76,15 +78,14 @@ class Mancala:
         
         pits = self.valid_moves()
         random_pit = random.randint(0, len(pits) - 1)
-        return pits[random_pit] + 1
+        return pits[random_pit] #Returns the random 0-index pit
     
     
     def play(self, pit):
         """
-        Simulates a single move made by a specific player using their selected pit.
+        Simulates a single move made by a specific player using their selected pit. Input is a 0-index pit.
         """
         
-        pit -= 1 # 1 based index  # Adjusts the pit index to 0-based
         if not self.valid_move(pit):
             print("INVALID MOVE")
             return -1 # Exits if the move is invalid
@@ -160,43 +161,33 @@ class Mancala:
         if res2 > res1: return 2  # Player 2 wins
         return 3  # It's a tie
 
-    def utility(self,val):
+    def utility(self,player=1):
         """
-        Calculates the utility of an outcome for a player based on an integer representing a terminal state.
+        Calculates the utility of an outcome for a player based on an integer representing a current state.
         """
         #TO-DO: See if this can/should be altered to work with difference in marbles rather than simply a winning state
 
         #Pros of just using terminal states: Easy, stops everything if a win is found
         #Pros of using difference in marbles: More complicated, may have to rework winning_eval or make a new method, could be much less expensive since a state of 32 to 4 may not be a winning state but is a good indicator of optimal play.
-        if self.current_player==1:
-            if val==1:
-                return 1
-            elif val==3:
-                return 0
-            else:
-                return -1
+        if player==1:
+            return self.board[self.p1_mancala_index] - self.board[self.p2_mancala_index]
         else:
-            if val==1:
-                return -1
-            elif val==3:
-                return 0
-            else:
-                return 1
+            return self.board[self.p2_mancala_index] - self.board[self.p1_mancala_index]
 
-    def max_value(self,default_state,alpha,beta):
+    def max_value(self,default_state,alpha,beta,depth,player):
         """
         Calculates the maximum value the player can get out of this state.
         """
         terminal = default_state.winning_eval() #Calculates whether this state is a final state
-        if terminal>0:
-            return self.utility(terminal) #Calculate the utility of this state for the player whose current turn it is
-        value = -2
+        if terminal>0 or depth==0:
+            return self.utility(player),-1 #Calculate the utility of this state for the player whose current turn it is
+        value = -1*(self.pits_per_player * self.stones_per_pit*2 + 1)
         pit_to_return = -1
-        pits = default_state.valid_moves() #Figure out what actions are possible
+        pits = default_state.valid_moves() #Figure out what actions are possible (0-index)
         for pit in pits: #For every action...
-            state = default_state #Reset state
-            state.play(pit) #Perform the action on that state
-            pit_val = self.min_value(state.play(pit),alpha,beta)#Examine value of that action what can be gained from that action
+            state = copy.deepcopy(default_state)#Clone the board state
+            state.play(pit) #Play move on this hypothetical board
+            pit_val, _ = self.min_value(state, alpha, beta,depth-1,player) #Determine value after making that move
             if pit_val>value: #If the worst move your opponent can force you into in this state is better than the worst move they can force you into in another state...
                 value = pit_val #Select this value as our best value
                 pit_to_return = pit #Bookmark the pit or action chosen
@@ -204,37 +195,37 @@ class Mancala:
             alpha = max(value,alpha) #Calculate new alpha if needed
         return value,pit_to_return #Return the value at this state
     
-    def min_value(self,default_state,alpha,beta):
+    def min_value(self,default_state,alpha,beta,depth,player):
         """
         Calculates the minimum value the player can get out of this state.
         """
         terminal = default_state.winning_eval() #Calculates whether this state is a final state
-        if terminal>0:
-            return self.utility(terminal) #Calculate the utility of this state for the player whose current turn it is
-        value = 2
+        if terminal>0 or depth==0:
+            return self.utility(player),-1 #Calculate the utility of this state for the player whose current turn it is
+        value = self.pits_per_player * self.stones_per_pit*2 + 1
         pit_to_return = -1
-        pits = default_state.valid_moves() #Figure out what actions are possible
+        pits = default_state.valid_moves() #Figure out what actions are possible (0-index)
         for pit in pits: #For every action...
-            state = default_state #Reset state
-            state.play(pit) #Perform that action on the state
-            pit_val = self.max_value(state,alpha,beta) #Examine value of that action what can be gained from that action
+            state = copy.deepcopy(default_state) #Clone the board state
+            state.play(pit) #Play move on this hypothetical board
+            pit_val, _ = self.max_value(state, alpha, beta,depth-1,player) #Determine value after making that move
             if pit_val<value: #If the best move the opponent can make in this state is worse than the best move they can make in another state...
                 value = pit_val #Select this value as our best value
                 pit_to_return = pit #Bookmark the pit or action chosen
             if value<=alpha: return value, pit_to_return #Prune if a value is found that is guaranteed to be chosen
-            beta = min(value,alpha) #Calculate new beta if needed
+            beta = min(value,beta) #Calculate new beta if needed
         return value, pit_to_return #Return the value at this state
 
 
-    def alphabeta_search(self):
+    def alphabeta_search(self,depth,player=1):
         """
         Performs AlphaBeta on the Mancala game.
         """
         #TO-DO: Decide whether to keep this as a member of the Mancala class OR make it separate
         #Pros of keeping it in: Less work and difference may be marginal
         #Pros of keeping it separate: More work but difference may be craazyyy
-        alpha = -2
-        beta = 2
-        state = self
-        value,pit = self.max_value(state,alpha,beta)
+        beta = self.pits_per_player * self.stones_per_pit*2 + 1
+        alpha = -beta
+        state = copy.deepcopy(self)
+        value,pit = self.max_value(state,alpha,beta,depth,player)
         return pit
